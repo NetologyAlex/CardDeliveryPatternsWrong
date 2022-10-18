a1946634276fa7e9f371de3cf43bd1cbd890bb40
NetologyAlex <96003016+NetologyAlex@users.noreply.github.com>
2022-10-18 11:51:42 +0300

    Update DataGenerator.java

--- src/test/java/ru/netology/data/DataGenerator.java
+++ src/test/java/ru/netology/data/DataGenerator.java
@@ -1,6 +1,8 @@
-package ru.netology.delivery.data;
+package ru.netology;
 
 import com.github.javafaker.Faker;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
@@ -9,42 +11,37 @@ import java.util.List;
 import java.util.Locale;
 import java.util.Random;
 
+@Data
+@NoArgsConstructor
 public class DataGenerator {
-    private DataGenerator() {
+
+    public static <T> T getRandom(List<T> list)
+    {
+        Random random = new Random();
+        return list.get(random.nextInt(list.size()));
     }
 
-    public static String generateDate(int days) {
-        // TODO: добавить логику для объявления переменной date и задания её значения, для генерации строки с датой
-        // Вы можете использовать класс LocalDate и его методы для получения и форматирования даты
-        String date = LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
-        return date;
+    public static String getRandomCity() {
+        List<String> list = Arrays.asList("Волгоград", "Брянск", "Москва", "Астрахань");
+        return getRandom(list);
     }
 
-    public static String generateCity() {
-        // TODO: добавить логику для объявления переменной city и задания её значения, генерацию можно выполнить
-        // с помощью Faker, либо используя массив валидных городов и класс Random
-        List<String> list = Arrays.asList("Архангельск", "Астрахань", "Барнаул", "Владикавказ",
-                "Горно-Алтайск", "Йошкар-Ола", "Казань", "Калининград", "Калуга", "Краснодар", "Махачкала",
-                "Нарьян-Мар", "Салехард", "Самара", "Саранск", "Саратов", "Хабаровск", "Ханты-Мансийск", "Южно-Сахалинск",
-                "Великий Новгород", "Владивосток", "Владимир", "Вологда", "Рязань", "Биробиджан", "Чебоксары", "Москва",
-                "Санкт-Петербург", "Ульяновск", "Симферополь", "Ростов-на-Дону");
-        Random rand = new Random();
-        String city = list.get(rand.nextInt(list.size()));
-        return city;
+    public static String getRandomCityInvalid() {
+        List<String> list = Arrays.asList("Камышин", "Волжский", "Балашиха", "Капустин Яр");
+        return getRandom(list);
     }
 
-    public static String generateName() {
-        // TODO: добавить логику для объявления переменной name и задания её значения, для генерации можно
-        // использовать Faker
+    public static String getDate(int day) {
+        return LocalDate.now().plusDays(day).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+    }
+
+    public static String getFakerName() {
         Faker faker = new Faker(new Locale("ru"));
         return faker.name().firstName() + " " + faker.name().lastName();
     }
 
-    public static String generatePhone() {
-        // TODO: добавить логику для объявления переменной phone и задания её значения, для генерации можно
-        // использовать Faker
+    public static String getFakerPhone() {
         Faker faker = new Faker(new Locale("ru"));
         return faker.phoneNumber().phoneNumber();
     }
-}
 }
